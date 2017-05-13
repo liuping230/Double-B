5bf9d64d4e34978c79798b03d5af0db4b2a5619b
liuping230 <810239165@qq.com>
2017-05-14 00:06:33 +0800

    完成书城的搜索界面的加载

--- app/src/main/java/com/lp/double_b/view/fragment/BookCityFragment.java
+++ app/src/main/java/com/lp/double_b/view/fragment/BookCityFragment.java
@@ -5,10 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.lp.double_b.R;
+import com.lp.double_b.view.activity.SearchActivity;
 
 public class BookCityFragment extends Fragment {
+
+    private LinearLayout searchLayout;
 
     public BookCityFragment() {
         // Required empty public constructor
@@ -17,8 +21,14 @@ public class BookCityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_city, container, false);
+        View view = inflater.inflate(R.layout.fragment_book_city, container, false);
+        searchLayout = (LinearLayout) view.findViewById(R.id.search_layout);
+        searchLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SearchActivity.startActivity(getActivity());
+            }
+        });
+        return view;
     }
-
-
 }
